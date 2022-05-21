1cf42a4428a4b75f58f12a6779bb548630f775db
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-22 00:16:32 +0600

    Mongodb Connected

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 //Middle Ware
 app.use(cors());
 app.use(express.json());
 
 //Mongodb Connect
+const uri = `mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.wu2yr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("mongo is connected");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!This is from Reyco Automotive");
