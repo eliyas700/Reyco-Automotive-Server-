beee7abab36a55ae947f6a52a52a11cefe88fc96
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-23 15:03:52 +0600

    Verify Throught JWT

--- index.js
+++ index.js
@@ -17,19 +17,59 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access!" });
+  }
+  //get the token from Auth header by Spliting
+  const token = authHeader.split(" ")[1];
+  //Verify Token (If it is Correct or not)
+  jwt.verify(token, process.env.MY_ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      // if Token is not Correct
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    //If token is Right
+    req.decoded = decoded;
+    console.log(decoded); // bar
+    next();
+  });
+};
 async function run() {
   try {
     await client.connect();
     const productsCollection = client
       .db("reyco-automotive")
       .collection("products");
+    const usersCollection = client.db("reyco-automotive").collection("users");
 
     //Get All The Products
-    app.get("/products", async (req, res) => {
+    app.get("/products", verifyJWT, async (req, res) => {
       const query = {};
       const cursor = productsCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
+    });
+
+    //Check Whether the user Was Previously logged in or Not
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign({ email: email }, process.env.MY_ACCESS_TOKEN, {
+        expiresIn: "7d",
+      });
+      res.send({ result, token });
     });
   } finally {
   }
