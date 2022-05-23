3b2f94ef74d5fc092839e69dd11ed53cbfac5b0f
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-23 14:40:47 +0600

    Get All Products From Db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-//Middle Ware
+const jwt = require("jsonwebtoken");
+const { decode } = require("jsonwebtoken");
+const port = process.env.PORT || 5000;
+//MiddleWare
 app.use(cors());
 app.use(express.json());
 
@@ -15,12 +17,24 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("mongo is connected");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client
+      .db("reyco-automotive")
+      .collection("products");
+
+    //Get All The Products
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!This is from Reyco Automotive");
